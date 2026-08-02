bf84976dd63ae2866275f5af05f15de41ec4f304
Mohammad Hammad <hammad.scripted@gmail.com>
2026-08-02 23:32:47 +0530

    feat: add routing and new pages for authentication, chat, home, and profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
-
+import { Route, Routes } from 'react-router-dom';
+import AuthPage from './pages/AuthPage';
+import ChatPage from './pages/ChatPage';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
 const App = () => {
   return (
-    <div className="text-3xl font-mono">App</div>
-  )
-}
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+      <Routes>
+        <Route>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/chat/:id" element={<ChatPage />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
