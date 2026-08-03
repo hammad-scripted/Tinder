941ff01de67872ae7a75480192bcf439084a55a8
Mohammad Hammad <hammad.scripted@gmail.com>
2026-08-03 23:37:36 +0530

    feat: update Sidebar component for improved match display and loading states, and add user seeding script for database initialization

--- client/src/store/useMatchStore.js
+++ client/src/store/useMatchStore.js
@@ -2,21 +2,38 @@ import { create } from 'zustand';
 import { axiosInstance } from '../lib/axios';
 import { toast } from 'react-hot-toast';
 export const useMatchStore = create((set) => ({
+  matches: [],
+  isLoadingMyMatches: false,
+  isLoadingUserProfiles: false,
+  userProfiles: [],
 
-
-    matches: [],
-    isLoadingMatches: false,
-
-    getMyMatches: async()=>{
-        try {
-            set({ isLoadingMatches: true });
-            const response = await axiosInstance.get('/matches');
-            set({ matches: response.data.matches, isLoadingMatches: false });
-        } catch (error) {
-            console.error('Get My Matches Error:', error);
-            set({ isLoadingMatches: false });
-            const errorMessage = error.response?.data?.message || 'Failed to get matches';
-            toast.error(errorMessage);
-        }   
+  getMyMatches: async () => {
+    try {
+      set({ isLoadingMyMatches: true });
+      const response = await axiosInstance.get('/matches');
+      set({ matches: response.data.matches, isLoadingMyMatches: false });
+    } catch (error) {
+      console.error('Get My Matches Error:', error);
+      set({ isLoadingMyMatches: false });
+      const errorMessage =
+        error.response?.data?.message || 'Failed to get matches';
+      toast.error(errorMessage);
     }
+  },
+  getUserProfiles: async () => {
+    try {
+      set({ isLoadingUserProfiles: true });
+      const response = await axiosInstance.get('/matches/user-profiles');
+      set({ userProfiles: response.data.users, isLoadingMatches: false });
+    } catch (error) {
+      console.error('Get User Profiles Error:', error);
+      set({ isLoadingUserProfiles: false, matches: [] });
+      const errorMessage =
+        error.response?.data?.message || 'Failed to get user profiles';
+      toast.error(errorMessage);
+    }
+  },
+  subscribeToNewMatches:async()=>{},
+
+  unsubscribeFromNewMatches:async()=>{},
 }));
