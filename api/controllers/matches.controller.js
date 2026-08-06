a046efc3733db21f433ae2b2e78a19fc91cd88c1
Mohammad Hammad <hammad.scripted@gmail.com>
2026-08-07 00:50:16 +0530

    updated user limit

--- api/controllers/matches.controller.js
+++ api/controllers/matches.controller.js
@@ -174,7 +174,7 @@ export const getUserProfiles = async (req, res) => {
       ],
     })
       .select('-password -email') // Hide sensitive fields
-      .limit(20); // Prevent loading thousands of users at once
+      .limit(50); // Prevent loading thousands of users at once
 
     return res.status(200).json({ success: true, count: users.length, users });
   } catch (err) {
